9b275efe8748ad8b5a6a5e8b1bbed25321bba9e7
a6st1n <juanagustin.sanchez26@gmail.com>
2026-05-26 14:05:09 -0300

    movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from player import Player
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+import player
 
 def main():        
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,16 +10,30 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill("black")
+    Player.containers = updatable, drawable
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
-        log_state()
+        log_state() # The tests need this to see your player's rotation!
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+
 
 
 
